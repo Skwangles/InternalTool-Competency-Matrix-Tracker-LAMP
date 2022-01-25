5f71aefa5cfd4e628a95acb383ff058a06122738
tree_house@hotmail.co.nz <G@2uA$3v2Ty>
2022-01-25 18:41:15 +1300

    Bootstrap link and script tags introduced - pre bootstrap commit

--- header.php
+++ header.php
@@ -6,12 +6,13 @@ session_start();
 <html lang="en" dir="ltr">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Competency Charts</title>
     <link rel="stylesheet" href="CSS/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="JS/update.ajax.js"></script>
-</head>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></head>
 
 <body>
 
@@ -20,8 +21,8 @@ session_start();
         <div class="wrapper">
             <ul>
                 <?php
-                if(isset($_SESSION["name"])){
-                echo "<p style=\"float:left;\"><b>User: ".$_SESSION["name"]."</b></p>";
+                if (isset($_SESSION["name"])) {
+                    echo "<p style=\"float:left;\"><b>User: " . $_SESSION["name"] . "</b></p>";
                 }
                 ?>
                 <a href="index.php">Home</a>
@@ -33,8 +34,8 @@ session_start();
                         echo "<a href=\"managerview.php\">Your Department's Competencies</a>";
                     }
                     if ($_SESSION["role"] == "3") { //If admin, shows admin options
-                        ?> 
-                        <a class="dropbtn" href="admin.php">Admin Panel</a>   
+                ?>
+                        <a class="dropbtn" href="admin.php">Admin Panel</a>
                 <?php
                     }
                     echo "<a href=\"settings.php\">Settings</a>";
