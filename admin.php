6e193bca6eb167924c4ecbd164348296dbc48f09
tree_house@hotmail.co.nz <G@2uA$3v2Ty>
2022-01-26 20:54:02 +1300

    added scroll to top code in footer, and fixed spelling mistake

--- admin.php
+++ admin.php
@@ -1,16 +1,11 @@
 <?php
 include_once 'header.php';
 
-//----------Not sure if this is secure, this is for the loading of the group list, have a look at alternative ways------
 if (!isset($_SESSION["role"]) || $_SESSION["role"] != "3") {
     header("location: index.php?error=invalidcall");
     exit();
 }
 ?>
-
-<!-- <br>
-<form class="centre" action="staffedit.php" method="post"><button class="actionbuttons addbuttons" type="submit" name="submit">Modify Staff Accounts</button></form>
-<form class="centre" action="gcedit.php" method="post"><button class="actionbuttons addbuttons" type="submit" name="submit">Modify Groups & Competencies</button></form> -->
 
 <hr class="seperator">
 <h1 class="centre">Staff Edit</h1>
--- footer.php
+++ footer.php
@@ -1,4 +1,28 @@
+<button onclick="topFunction()" id="myBtn" title="Go to top">Go To Top</button>
 </body>
+
+<script>//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+</script>
 <script src="JS/javascript.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </html>
